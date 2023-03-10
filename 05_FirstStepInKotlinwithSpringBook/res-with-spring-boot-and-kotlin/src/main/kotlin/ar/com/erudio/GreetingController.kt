243c07ecb8894879eb28548b36e4bb2eeb95f62e
Juan <juankenci@gmail.com>
2023-03-10 18:06:36 -0300

    Segunda subida

--- 05_FirstStepInKotlinwithSpringBook/res-with-spring-boot-and-kotlin/src/main/kotlin/ar/com/erudio/GreetingController.kt
+++ 05_FirstStepInKotlinwithSpringBook/res-with-spring-boot-and-kotlin/src/main/kotlin/ar/com/erudio/GreetingController.kt
@@ -1,6 +1,7 @@
 package ar.com.erudio
 
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
@@ -9,7 +10,7 @@ class GreetingController {
     val counter:AtomicLong= AtomicLong();
 
     @RequestMapping("/greeting")
-    fun greeting():Greeting{
-        return Greeting(counter.incrementAndGet(),"Hola Mundo!");
+    fun greeting(@RequestParam(value="name", defaultValue = "Mundo") name:String?):Greeting{
+        return Greeting( counter.incrementAndGet(),"Hola $name!");
     }
 }
